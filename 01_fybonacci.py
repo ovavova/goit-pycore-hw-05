61845840cd3c1dbece0ecff99c60fcc206e7507f
Ovavova <vyereschenko@gmail.com>
2025-10-27 19:33:07 +0200

    01 additional

--- 01_fybonacci.py
+++ 01_fybonacci.py
@@ -29,16 +29,18 @@ print(fib(10))
 print(fib(15))
 
 # testing faster cache 
+iterations = 900 # Setting datapoint 
 
 start_1 = time.time()
-print(fib(400))
+print(fib(iterations))
 end_1 = time.time()
 
 start_2 = time.time()
-print(fib(400))
+print(fib(iterations))
 end_2 = time.time()
 
-# Just for fun :)
-print(f"Execution time 1: {(end_1 - start_1)*(10**6):0.2f} microseconds. Execution time 2:{(end_2 - start_2)*(10**6):0.2f} microseconds")
+# Comparing camputation times for cashed vs non cached  
+print("-|"*iterations)
+print(f"Execution time no-cache: {(end_1 - start_1)*(10**6):0.2f} microseconds. Execution time using cache:{(end_2 - start_2)*(10**6):0.2f} microseconds")
 print(f"time delta: {((end_1 - start_1)-(end_2 - start_2))*10**6:0.2f} microseconds")
-print(f"Faster exec {((end_1 - start_1)/(end_2 - start_2))*10**6:_} times")  
+print(f"Cashed access faster on {iterations} iterations  {((end_1 - start_1)/(end_2 - start_2)):.0f} times")  
