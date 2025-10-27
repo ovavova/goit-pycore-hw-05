e715fd917cec2222c09b24898278ffebaff8595d
Ovavova <vyereschenko@gmail.com>
2025-10-27 20:21:00 +0200

    02 and formating

--- 01_fybonacci.py
+++ 01_fybonacci.py
@@ -2,45 +2,54 @@
 Реалізуйте функцію caching_fibonacci, яка створює та використовує кеш для зберігання і повторного використання вже обчислених значень чисел Фібоначчі.
 """
 
-import time                                   # To test faster cache
+import time  # To test faster cache
+
 
 def cashing_fibonacci():
-    cache = {}                                # Cache dictionary
+    cache = {}  # Cache dictionary
 
     def fibonacci(n):
         if n <= 0:
             return 0
-        if n == 1:                            # Base case
+        if n == 1:  # Base case
             return 1
         if n in cache:
-            return cache[n]                   # Returns already calculated value from dictionary cahce for number-key "n"
-        
-        cache[n] = fibonacci(n-1) + fibonacci(n-2) # Рекурсія
+            return cache[n]  # Returns already calculated value from dictionary cahce for number-key "n"
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)  # Рекурсія
         return cache[n]
-    
-    return fibonacci                               # Повертаємо внутрішню функцію
-        
 
-# testing
+    return fibonacci  # Повертаємо внутрішню функцію
 
-fib = cashing_fibonacci()
 
-print(fib(10))
-print(fib(15))
+if __name__ == "__main__":
 
-# testing faster cache 
-iterations = 900 # Setting datapoint 
+    # testing
 
-start_1 = time.time()
-print(fib(iterations))
-end_1 = time.time()
+    fib = cashing_fibonacci()
 
-start_2 = time.time()
-print(fib(iterations))
-end_2 = time.time()
+    print(fib(10))
+    print(fib(15))
 
-# Comparing camputation times for cashed vs non cached  
-print("-|"*iterations)
-print(f"Execution time no-cache: {(end_1 - start_1)*(10**6):0.2f} microseconds. Execution time using cache:{(end_2 - start_2)*(10**6):0.2f} microseconds")
-print(f"time delta: {((end_1 - start_1)-(end_2 - start_2))*10**6:0.2f} microseconds")
-print(f"Cashed access faster on {iterations} iterations  {((end_1 - start_1)/(end_2 - start_2)):.0f} times")  
+    # testing faster cache
+    iterations = 900  # Setting datapoint
+
+    start_1 = time.time()
+    print(fib(iterations))
+    end_1 = time.time()
+
+    start_2 = time.time()
+    print(fib(iterations))
+    end_2 = time.time()
+
+    # Comparing camputation times for cashed vs non cached
+    print("-|" * iterations)
+    print(
+        f"Execution time no-cache: {(end_1 - start_1)*(10**6):0.2f} microseconds. Execution time using cache:{(end_2 - start_2)*(10**6):0.2f} microseconds"
+    )
+    print(
+        f"time delta: {((end_1 - start_1)-(end_2 - start_2))*10**6:0.2f} microseconds"
+    )
+    print(
+        f"Cashed access faster on {iterations} iterations  {((end_1 - start_1)/(end_2 - start_2)):.0f} times"
+    )
---
+++ 02_generator_numbers.py
@@ -0,0 +1,26 @@
+import re
+
+
+def generator_numbers(text: str):
+    pattern = r"[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?"  # Pattern for real numbers
+
+    for match in re.finditer(
+        pattern, text
+    ):  # ItarationIterating via object of patterns
+        yield float(match.group())
+
+
+def sum_profit(text: str, func: callable):
+    sum_profit = 0
+    for num in generator_numbers(text):  # Calling iterator fucntion
+        sum_profit += num  # adding next real number
+    return sum_profit
+
+
+# test
+if __name__ == "__main__":
+
+    text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")
+
